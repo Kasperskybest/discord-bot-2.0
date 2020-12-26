9147e78f98e9460e65c8c7cfee0054ec2c5f6cab
Sushipie <67330141+Sushipie@users.noreply.github.com>
2020-12-26 09:29:41 +0500

    Reolved antispam issue

--- events/antiSpam.js
+++ events/antiSpam.js
@@ -1,8 +1,8 @@
 const AntiSpam = require("discord-anti-spam");
 const antiSpam = new AntiSpam({
-  warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
-  kickThreshold: 7, // Amount of messages sent in a row that will cause a ban.
-  banThreshold: 7, // Amount of messages sent in a row that will cause a ban.
+  warnThreshold: 15, // Amount of messages sent in a row that will cause a warning.
+  kickThreshold: 30, // Amount of messages sent in a row that will cause a ban.
+  banThreshold: 40, // Amount of messages sent in a row that will cause a ban.
   maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
   warnMessage: "{@user}, Please stop spamming.", // Message that will be sent in chat upon warning a user.
   kickMessage: "**{user_tag}** has been kicked for spamming.", // Message that will be sent in chat upon kicking a user.
