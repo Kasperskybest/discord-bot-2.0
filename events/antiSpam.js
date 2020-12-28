1fc52f56eeda662b4e389389de40e5dc1a03c6e2
Sushipie <67330141+Sushipie@users.noreply.github.com>
2020-12-28 09:20:49 +0500

    Redcued time interval for antispam

--- events/antiSpam.js
+++ events/antiSpam.js
@@ -3,7 +3,7 @@ const antiSpam = new AntiSpam({
   warnThreshold: 15, // Amount of messages sent in a row that will cause a warning.
   kickThreshold: 30, // Amount of messages sent in a row that will cause a ban.
   banThreshold: 40, // Amount of messages sent in a row that will cause a ban.
-  maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
+  maxInterval: 1000, // Amount of time (in milliseconds) in which messages are considered spam.
   warnMessage: "{@user}, Please stop spamming.", // Message that will be sent in chat upon warning a user.
   kickMessage: "**{user_tag}** has been kicked for spamming.", // Message that will be sent in chat upon kicking a user.
   banMessage: "**{user_tag}** has been banned for spamming.", // Message that will be sent in chat upon banning a user.
